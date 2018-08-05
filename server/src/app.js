544ec8fd79ebc53519b1435de173131afa0f6e0d
Zach Jones <zachjones@Zachs-MacBook-Pro-4.local>
2018-08-05 09:41:19 -0700

    install-5

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,20 @@
-console.log('hello')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+//  USE MORGAN (LOG GENERATOR)
+app.use(morgan('combined'))
+//  ALLOW EXPRESS APP TO EASILY PARSE JSON
+app.use(bodyParser.json())
+//  ALLOW ANY CLIENT TO ACCESS THIS WEBSITE
+app.use(cors())
+
+app.post('/register', (req, res) => {
+  res.send({
+    message: `Hello ${req.body.email}! Your user was registered!`
+  })
+})
+
+app.listen(process.env.PORT || 8081)
